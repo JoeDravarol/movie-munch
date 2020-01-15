81fc4dad523bc02217a1b7944e93f636f04cfe9a
Joe <joedravarol@gmail.com>
2020-01-15 00:17:54 +0000

    Implement active style to change on navigation links
    Navigation Link will change to active accoriding to the current path
    the browser is on.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,24 +3,25 @@ import Nav from './Nav'
 import UlElem from './UlElem'
 import ListElem from './ListElem'
 import Link from './Link'
+import { withRouter } from 'react-router-dom'
 
-const Menu = (props) => {
+const Menu = ({ location }) => {
   return (
     <Nav>
       <UlElem>
-        <ListElem active>
+        <ListElem active={location.pathname === '/'}>
           <Link to="/">Discover</Link>
         </ListElem>
-        <ListElem>
+        <ListElem active={location.pathname === '/now_playing'}>
           <Link to="/now_playing">Now Playing</Link>
         </ListElem>
-        <ListElem>
+        <ListElem active={location.pathname === '/popular'}>
           <Link to="/popular">Popular</Link>
         </ListElem>
-        <ListElem>
+        <ListElem active={location.pathname === '/upcoming'}>
           <Link to="/upcoming">Upcoming</Link>
         </ListElem>
-        <ListElem>
+        <ListElem active={location.pathname === '/top_rated'}>
           <Link to="/top_rated">Top Rated</Link>
         </ListElem>
       </UlElem>
@@ -28,4 +29,4 @@ const Menu = (props) => {
   )
 }
 
-export default Menu
+export default withRouter(Menu)
