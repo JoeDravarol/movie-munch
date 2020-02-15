553f3b3dc5c0e02d46d2c8926772099c357c62b8
Joe <joedravarol@gmail.com>
2020-02-15 14:59:28 +0000

    Add Logo and Tmdb Logo

--- src/components/Menu/Nav.js
+++ src/components/Menu/Nav.js
@@ -3,4 +3,6 @@ import styled from 'styled-components'
 export default styled.nav`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
 `
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,27 +4,49 @@ import UlElem from './UlElem'
 import ListElem from './ListElem'
 import Link from './Link'
 import { withRouter } from 'react-router-dom'
+import styled from 'styled-components'
+
+import TmdbLogo from './TmdbLogo'
+
+const Logo = styled.h1`
+  color: #F83646;
+  text-align: center;
+  font-size: 3.5em;
+  margin-top: 10px;
+  line-height: 1;
+
+  span {
+    display: block;
+  }
+`
 
 const Menu = ({ location }) => {
   return (
     <Nav>
-      <UlElem>
-        <ListElem active={location.pathname === '/'}>
-          <Link to="/">Discover</Link>
-        </ListElem>
-        <ListElem active={location.pathname === '/now_playing'}>
-          <Link to="/now_playing">Now Playing</Link>
-        </ListElem>
-        <ListElem active={location.pathname === '/popular'}>
-          <Link to="/popular">Popular</Link>
-        </ListElem>
-        <ListElem active={location.pathname === '/upcoming'}>
-          <Link to="/upcoming">Upcoming</Link>
-        </ListElem>
-        <ListElem active={location.pathname === '/top_rated'}>
-          <Link to="/top_rated">Top Rated</Link>
-        </ListElem>
-      </UlElem>
+      <header>
+        <Logo>
+          Movie<span>Munch</span> 
+        </Logo>
+        <UlElem>
+          <ListElem active={location.pathname === '/'}>
+            <Link to="/">Discover</Link>
+          </ListElem>
+          <ListElem active={location.pathname === '/now_playing'}>
+            <Link to="/now_playing">Now Playing</Link>
+          </ListElem>
+          <ListElem active={location.pathname === '/popular'}>
+            <Link to="/popular">Popular</Link>
+          </ListElem>
+          <ListElem active={location.pathname === '/upcoming'}>
+            <Link to="/upcoming">Upcoming</Link>
+          </ListElem>
+          <ListElem active={location.pathname === '/top_rated'}>
+            <Link to="/top_rated">Top Rated</Link>
+          </ListElem>
+        </UlElem>
+      </header>
+      
+      <TmdbLogo />
     </Nav>
   )
 }
